a177e2d1bf43394c1018ad9d75e2bc7e25b75f54
Danvass <politystaff@gmail.com>
2023-10-11 20:27:06 -0400

    rename files for clarity

---
+++ slope/src/createUser.js
@@ -0,0 +1,10 @@
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+
+/*Takes information from body and sends POST request to create user in spreadsheet
+  Returns: (bool) True if successful entry into spreadsheet, false if not
+*/
+const createUser = async (body)  => {
+  //TODO
+}
+
+export default createUser;
