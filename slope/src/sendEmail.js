b58fc56718cca27e4e97a6ca3b97ab3397035e83
Danvass <politystaff@gmail.com>
2023-10-11 20:11:30 -0400

    Dependencies and Boilerplate

---
+++ slope/src/sendEmail.js
@@ -0,0 +1,10 @@
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+
+/*Takes information from body and sends POST request
+  Returns: (bool) True if successful entry into spreadsheet, false if not
+*/
+const sendEmail = async (body)  => {
+  //TODO
+}
+
+export default sendEmail;
